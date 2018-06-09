7026ef1e3d9191593169f51be954c1f4831721ba
‘Tim <timothy.j.ocampo@gmail.com>
2018-06-08 21:34:12 -0700

    Refactored code : <canvas> DOM element is now created in the index.js file

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-//define the canvas
-var canvas = document.getElementById('canvasSpace');
+// //define the canvas
+// var canvas = document.getElementById('canvasSpace');
+//
+// //sets interaction with the canvas
+// var ctx = canvas.getContext("2d");
+//
+// //Test to see if this js talks to the canvas : param2 = x axis, param1 = y axis
+// ctx.fillText("Hello World", 50, 150);
 
-//sets interaction with the canvas
+
+//Refactored
+
+var canvas = document.createElement("canvas"); //same thing as <canvas></canvas>
 var ctx = canvas.getContext("2d");
 
-//Test to see if this js talks to the canvas : param2 = x axis, param1 = y axis
-ctx.fillText("Hello World", 50, 150);
+canvas.height = 400;
+canvas.width = 600;
+
+document.body.appendChild(canvas); //stick it in the dom
+
+ctx.fillText("Hello World", 50, 150); // this line has to come AFTER canvas.height && width
