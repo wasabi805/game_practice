582ef11d85667dcf0a5935f337d9bd8118c5ba9d
‘Tim <timothy.j.ocampo@gmail.com>
2019-02-13 13:20:43 -0800

    fixed typo on const port in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
-const port = process.env.PORT|| 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log( `Server running on port ${port}` );
 });
